04984ab2236d103c0d8887b1ddd9228ed6045b39
Tyler Robinson <tydiz936@gmail.com>
2010-06-11 13:17:04 -0600

    Added this class of methods that allow the Article class to handle dates correctly

---
+++ src/com/AA/Other/DateFunctions.java
@@ -0,0 +1,199 @@
+package com.AA.Other;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+/***
+ * Bunch of functions for handling dates that represent an article
+ * 
+ * @author Tyler Robinson
+ * 
+ */
+public class DateFunctions {
+	/***
+	 * Given a 3 character month name, the month number is returned
+	 * 
+	 * I hate this code and proves the need for enumerations, but since
+	 * those don't exist in java(or I don't think they do), this is as close
+	 * as we can get(unless we defined a dictionary of some sort...but meh)
+	 * 
+	 * @param month - 3 character month name, i.e. jan, or feb
+	 * @return The number that this represents the month
+	 */
+	public static int getMonth(String month) {
+		if(month.equals("Jan"))
+			return 1;
+		else if(month.equals("Feb"))
+			return 2;
+		else if(month.equals("Mar"))
+			return 3;
+		else if(month.equals("Apr"))
+			return 4;
+		else if(month.equals("May"))
+			return 5;
+		else if(month.equals("Jun"))
+			return 6;
+		else if(month.equals("Jul"))
+			return 7;
+		else if(month.equals("Aug"))
+			return 8;
+		else if(month.equals("Sep"))
+			return 9;
+		else if(month.equals("Oct"))
+			return 10;
+		else if(month.equals("Nov"))
+			return 11;
+		else
+			return 12;
+	}
+	
+	/***
+	 * Given a month number, the month name is returned
+	 * 
+	 * I hate this code and proves the need for enumerations, but since
+	 * those don't exist in java(or I don't think they do), this is as close
+	 * as we can get(unless we defined a dictionary of some sort...but meh)
+	 * 
+	 * @param month - the number of the month to be converted
+	 * @return The name of the given month 
+	 */
+	public static String getMonth(int month) {
+		if(month == 1)
+			return "January";
+		else if(month == 2)
+			return "February";
+		else if(month == 3)
+			return "March";
+		else if(month == 4)
+			return "April";
+		else if(month == 5)
+			return "May";
+		else if(month == 6)
+			return "June";
+		else if(month == 7)
+			return "July";
+		else if(month == 8)
+			return "August";
+		else if(month == 9)
+			return "September";
+		else if(month == 10)
+			return "October";
+		else if(month == 11)
+			return "November";
+		else
+			return "December";
+	}
+	
+	/***
+	 * Creates a Calendar object based off a string with this format
+	 * 
+	 * <Day name:3 letters>, DD <Month name: 3 letters> YYYY HH:MM:SS +0000 <---What is this?? Idk, just ignore
+	 * @param date - The string we are to convert to a calendar date
+	 * @return - The calendar date object that this string represents
+	 */
+	public static Calendar makeDate(String date) {
+		Scanner reader = new Scanner(date);
+		reader.next();
+		
+		int day = reader.nextInt();		
+		int month = DateFunctions.getMonth(reader.next())-1;		
+		int year = reader.nextInt();
+		
+		//Split the time HH:MM:SS into [HH][MM][SS]
+		String[] time = reader.next().split(":");		
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(year, month, day,Integer.parseInt(time[0]),Integer.parseInt(time[1]));
+		return calendar;
+	}
+	
+	/***
+	 * Compares the 2 calendars against the given field for equality
+	 * @param field - integer that represents the field to be compared(i.e. Calendar.MONTH)
+	 * @param timeA - A calendar object to be compared
+	 * @param timeB - A calendar object to be compared
+	 * @return true if the fields return the same number, false otherwise
+	 */
+	public static boolean isDateFieldEqual(int field, Calendar timeA, Calendar timeB) {
+		if(timeA.get(field) == timeB.get(field))
+			return true;
+		
+		return false;
+	}
+	
+	/***
+	 * Takes the date and creates an intelligent string response about
+	 * the difference of the current time and the given time
+	 * 
+	 * For example, if the article was posted 2 hours before the current time
+	 * the response would be "About 2 hours ago" but if the article was posted
+	 * a day before the current time, the response would be "Yesterday"
+	 * 
+	 * Sorry about how nasty this code is...I don't know if there can be
+	 * an improvements to it, seeing that there really isn't much flexability
+	 * with these Calendar objects
+	 * 
+	 * @param articleDate - The date of posting
+	 * @return An intelligent response of the difference between the current
+	 * time and the article date
+	 */
+	public static String convertToString(Calendar articleDate) {
+		Calendar currentTime = Calendar.getInstance();
+		
+		String printDate = "";
+		
+		//If the date is the same as when published then return
+		//a time difference; If the date is one day more than when it
+		//was published, then return "Yesterday"; Otherwise return the
+		//formated date: <Month full name> <Day>, <Year>
+		if(isDateFieldEqual(Calendar.MONTH,articleDate,currentTime) &&
+				isDateFieldEqual(Calendar.DAY_OF_MONTH,articleDate,currentTime) &&
+				isDateFieldEqual(Calendar.YEAR,articleDate,currentTime))
+		{
+			//If the hour is the same...return how many minutes ago the article was published
+			//Otherwise return how many hours ago the article was published
+			if(isDateFieldEqual(Calendar.HOUR,articleDate,currentTime))
+				printDate = "About " + (currentTime.get(Calendar.MINUTE) - 
+						articleDate.get(Calendar.MINUTE)) + " mins ago"; 
+			else
+				printDate = "About " + (currentTime.get(Calendar.HOUR) - 
+						articleDate.get(Calendar.HOUR)) + " hours ago";
+		}
+		else if((isDateFieldEqual(Calendar.YEAR,articleDate,currentTime) &&
+				currentTime.get(Calendar.DAY_OF_YEAR) - articleDate.get(Calendar.DAY_OF_YEAR) == 1)||
+				isCrossingYears(articleDate,currentTime))
+			printDate = "Yesterday";
+		else	
+			printDate = DateFunctions.getMonth(articleDate.get(Calendar.MONTH)+1) + " " +
+					articleDate.get(Calendar.DAY_OF_MONTH) + ", " + articleDate.get(Calendar.YEAR);
+		
+		return printDate;
+	}
+	
+	/***
+	 * This class just checks to see the given 2 times are crossing over 
+	 * the year mark(Article Date = Dec 31 and Current Date = Jan 1)
+	 * 
+	 * @param articleDate - Day the article was posted
+	 * @param currentDate - The current day
+	 * @return - True if articleDate = Dec 31 Year = n and currentDate = Jan 1 Year = n + 1
+	 */
+	public static boolean isCrossingYears(Calendar articleDate, Calendar currentDate) {
+		if(currentDate.get(Calendar.YEAR) - articleDate.get(Calendar.YEAR) != 1)
+			return false;
+		
+		Calendar lastDayOfDec = (Calendar)articleDate.clone();
+		Calendar firstDayOfJan = (Calendar)currentDate.clone();
+		
+		lastDayOfDec.set(Calendar.MONTH,Calendar.DECEMBER);
+		lastDayOfDec.set(Calendar.DAY_OF_MONTH, 31);
+		
+		firstDayOfJan.set(Calendar.MONTH, Calendar.JANUARY);
+		firstDayOfJan.set(Calendar.DAY_OF_MONTH, 1);
+		
+		if(articleDate.equals(lastDayOfDec) && currentDate.equals(firstDayOfJan))
+			return true;
+ 		
+		return false;
+	}
+}
